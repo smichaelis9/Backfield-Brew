fe31c3b1c94828890be016402fd3b994919e82db
smichaelis9 <107019096+smichaelis9@users.noreply.github.com>
2026-05-02 22:31:51 -0500

    Update patreon-auth.js

--- netlify/functions/patreon-auth.js
+++ netlify/functions/patreon-auth.js
@@ -1,1 +1,11 @@
+exports.handler = async () => {
+  const clientId = process.env.PATREON_CLIENT_ID;
+  const redirectUri = process.env.PATREON_REDIRECT_URI;
 
+  return {
+    statusCode: 302,
+    headers: {
+      Location: `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=identity identity.memberships`
+    }
+  };
+};
